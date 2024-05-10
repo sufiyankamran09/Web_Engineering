c544a7c16ee5d750f48ae917de893828caf712ae
Sufiyan <sufiyankamran09@gmail.com>
2024-05-10 20:14:12 +0500

    first commit

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -51,8 +51,10 @@ export async function POST(req: Request) {
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+
+  if (evt.type === 'user.created') {
+    console.log('userId:', evt.data.id)
+  }
 
   return new Response('', { status: 200 })
 }
